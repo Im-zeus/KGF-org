2a955378ff0aadcba0fba607e7d6aa2d84220375
zues <87465532+Im-zeus@users.noreply.github.com>
2021-08-11 17:52:00 +0530

    Create __forcesubs.py

--- LaylaRobot/modules/__forcesubs.py
+++ LaylaRobot/modules/__forcesubs.py
@@ -181,7 +181,7 @@ def config(client, message):
 
 __help__ = """
 *Force Subscribe:*
-❍ Layla can mute members who are not subscribed your channel until they subscribe
+❍ I can mute members who are not subscribed your channel until they subscribe
 ❍ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
 *Setup*
 *Only creator*
@@ -196,4 +196,4 @@ __help__ = """
   💡If you disable fsub, you need to set again for working.. /fsub {channel username} 
  ❍ /fsub clear - To unmute all members who muted by me.
 """
-__mod_name__ = "F-Sub"
+__mod_name__ = "🔱F-SUB🔱"

c60f395e46a86e24519950a33fc4122cc473ccb9
zues <87465532+Im-zeus@users.noreply.github.com>
2021-08-11 18:19:32 +0530

    Create log_channel.py

--- LaylaRobot/modules/log_channel.py
+++ LaylaRobot/modules/log_channel.py
@@ -203,7 +203,7 @@ Setting the log channel is done by:
 ❍ forwarding the /setlog to the group
 """
 
-    __mod_name__ = "Channel"
+    __mod_name__ = "🔱CHANNEL🔱"
 
     LOG_HANDLER = CommandHandler("logchannel", logging)
     SET_LOG_HANDLER = CommandHandler("setlog", setlog)

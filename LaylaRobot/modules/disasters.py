b13906b7a2949e16696bb861437f76a6ce5553bb
zues <87465532+Im-zeus@users.noreply.github.com>
2021-08-11 18:12:22 +0530

    Create disasters.py

--- LaylaRobot/modules/disasters.py
+++ LaylaRobot/modules/disasters.py
@@ -678,7 +678,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "Dev"
+__mod_name__ = "🔱DEV🔱"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,

86df7aef72765069f02e758fbed4c70f6595b645
zues <87465532+Im-zeus@users.noreply.github.com>
2021-08-11 18:21:34 +0530

    Create logomaker.py

--- LaylaRobot/modules/logomaker.py
+++ LaylaRobot/modules/logomaker.py
@@ -100,7 +100,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByLayla.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @LaylaList Support @AwesomeSupport")
+    await tbot.send_file(event.chat_id, fname2, caption="[Made By KGF](t.me/noobiezhub)")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
@@ -140,7 +140,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "LogoByLyala.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @LaylaRobot")
+    await tbot.send_file(event.chat_id, fname2, caption="[Made By KGF](t.me/noobiezhub)")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
@@ -156,4 +156,4 @@ __help__ = """
  ❍ /wlogo text :  Create your logo with your name
 
  """
-__mod_name__ = "Logo"
+__mod_name__ = "🔱LOGO🔱"

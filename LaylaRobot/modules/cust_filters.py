c3d5c24a8237db450bf1640ada0ff608a76af716
zues <87465532+Im-zeus@users.noreply.github.com>
2021-08-11 18:09:36 +0530

    Create cust_filters.py

--- LaylaRobot/modules/cust_filters.py
+++ LaylaRobot/modules/cust_filters.py
@@ -639,7 +639,7 @@ Check ❍ /markdownhelp to know more!
 
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "🔱FILTERS🔱"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)

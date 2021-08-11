5f072d22a76bf95269b02f0b7bbe7db84875fd8a
zues <87465532+Im-zeus@users.noreply.github.com>
2021-08-11 18:38:34 +0530

    Create speed_test.py

--- LaylaRobot/modules/speed_test.py
+++ LaylaRobot/modules/speed_test.py
@@ -59,6 +59,6 @@ SPEED_TEST_CALLBACKHANDLER = CallbackQueryHandler(
 dispatcher.add_handler(SPEED_TEST_HANDLER)
 dispatcher.add_handler(SPEED_TEST_CALLBACKHANDLER)
 
-__mod_name__ = "🔱SPEEDTEST🔱"
+__mod_name__ = "🔱SPEED TEST🔱"
 __command_list__ = ["speedtest"]
 __handlers__ = [SPEED_TEST_HANDLER, SPEED_TEST_CALLBACKHANDLER]

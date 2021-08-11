d4f13d6189358bfea718b685aa1d2191e34289bb
zues <87465532+Im-zeus@users.noreply.github.com>
2021-08-11 17:56:26 +0530

    Create __songs.py

--- LaylaRobot/modules/__songs.py
+++ LaylaRobot/modules/__songs.py
@@ -146,7 +146,7 @@ async def deezer(_, message):
     await m.delete()
     
     
-__mod_name__ = "◎Music"
+__mod_name__ = "🔱MUSIC🔱"
 
 __help__ = """
 • `/song`** <songname artist(optional)>: download the song in it's best quality available.(API BASED)

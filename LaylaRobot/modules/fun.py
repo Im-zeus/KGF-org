e14ce2d1ee86501cb7e574f98a21ed10547a4187
zues <87465532+Im-zeus@users.noreply.github.com>
2021-08-11 18:15:10 +0530

    Create fun.py

--- LaylaRobot/modules/fun.py
+++ LaylaRobot/modules/fun.py
@@ -399,7 +399,7 @@ dispatcher.add_handler(TABLE_HANDLER)
 dispatcher.add_handler(GDMORNING_HANDLER)
 dispatcher.add_handler(GDNIGHT_HANDLER)
 
-__mod_name__ = "Memes"
+__mod_name__ = "🔱MEMES🔱"
 __command_list__ = [
     "runs",
     "slap",

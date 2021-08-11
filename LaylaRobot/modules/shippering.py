0b6483338f52cd4e9351ff13b150be9ae451407d
zues <87465532+Im-zeus@users.noreply.github.com>
2021-08-11 18:37:36 +0530

    Create shippering.py

--- LaylaRobot/modules/shippering.py
+++ LaylaRobot/modules/shippering.py
@@ -79,5 +79,5 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
 __help__ = """
  ❍ /couples - To Choose Couple Of The Day
  """
-__mod_name__ = "Couples"
+__mod_name__ = "🔱COUPLES🔱"
 
